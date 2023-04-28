73fc4005abfc175bacf91fe1f56b155b71643c46
Ivan Jose Laverde Mesa <41165667+ijlm@users.noreply.github.com>
2023-04-28 13:38:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -67,5 +67,5 @@ async def generate_chart(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
     
